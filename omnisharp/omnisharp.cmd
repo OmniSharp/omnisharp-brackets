43f6e7c13538ef9845450cbfbc0a7dd5c374ae66
Mat McLoughlin <mathew.mcloughlin@gmail.com>
2015-02-04 21:04:10 +0000

    fixing omnisharp on windows

--- omnisharp/omnisharp.cmd
+++ omnisharp/omnisharp.cmd
@@ -1,2 +1,1 @@
-
-@"%~dp0approot\packages\KRE-Mono.1.0.0-beta2\bin\klr.exe" --appbase "%~dp0approot\packages\OmniSharp\1.0.0\root" Microsoft.Framework.ApplicationHost omnisharp %*
+@"%~dp0approot\packages\KRE-CLR-x86.1.0.0-beta2\bin\klr.exe" --appbase "%~dp0approot\packages\OmniSharp\1.0.0\root" Microsoft.Framework.ApplicationHost omnisharp %*
